dfa085aa3f70d93bf4142f59f2108072f23bc8ea
Diego <diego@fmpwizard.com>
2011-03-22 01:13:35 -0400

    Minor changes

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -8,6 +8,9 @@ import common._
 import http._
 import sitemap._
 import Loc._
+import mapper._
+
+//import code.model._
 
 
 /**
@@ -16,21 +19,42 @@ import Loc._
  */
 class Boot {
   def boot {
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor = 
+	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+			     Props.get("db.url") openOr 
+			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			     Props.get("db.user"), Props.get("db.password"))
+
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
+
+    // Use Lift's Mapper ORM to populate the database
+    // you don't need to use Mapper to use Lift... use
+    // any ORM you want
+    Schemifier.schemify(true, Schemifier.infoF _)
+
     // where to search snippet
-    LiftRules.addToPackages("code")
+    LiftRules.addToPackages("com.fmpwizard.code")
 
     // Build SiteMap
-    val entries = List(
-      Menu.i("Home") / "index", // the simple way to declare a menu
+    def sitemap = SiteMap(
+      Menu.i("Home") / "index" ,
+      Menu.i("Ajax Form") / "ajax-form" ,
+      Menu.i("Wiring") / "wiring" ,
 
       // more complex because this menu allows anything in the
       // /static path to be visible
       Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
 	       "Static Content")))
 
+
+
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    LiftRules.setSiteMapFunc(() =>sitemap)
 
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
@@ -43,5 +67,13 @@ class Boot {
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
+
+
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))    
+
+    // Make a transaction span the whole HTTP request
+    S.addAround(DB.buildLoanWrapper)
   }
 }
