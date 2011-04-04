c2177586b2947cf0632ab3f87efabe54220a1e84
Diego <diego@fmpwizard.com>
2011-04-04 17:18:35 -0400

    Yes! I got the reactive web example working, now I just need to make the yeild code pretty :)

--- src/main/scala/code/snippet/reactiveweb.scala
+++ src/main/scala/code/snippet/reactiveweb.scala
@@ -49,10 +49,8 @@ class reactiveweb extends Logger{
   val selectedCitySignal: Signal[Option[String]] = for{
     cities <- validCitiesSignal
     i <- citiesSelect.selectedIndex.value
-    //i <- 0
-  //} yield {println("Diegoooooooooooo "  + cities.size +  " sss " + cities); if(cities.size <= i.getOrElse(0)){println("qqqqqq"); Some(0) map cities} else{   i map cities} }
-  //} yield  if(cities.size <= i.getOrElse(0)){ Some(0) map cities} else{   i map cities } 
-  } yield i map cities
+  } yield  if(cities.size <= i.getOrElse(0)){ citiesSelect.selectedIndex.value ()= Some(0); Some(0) map cities} else {   i map cities } 
+  //} yield i map cities
 
   val validIdsSignal = selectedCitySignal.map(_.toList flatMap CitiesAndStates3.idsFor)
   val idsSelect = Select(validIdsSignal)
